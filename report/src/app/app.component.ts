fbb732ee02b6aa4c7b326a7954717cac03fd576d
adamaso <angela.damaso@kaplan.com>
2017-05-09 16:57:38 -0400

    WIP refactor

--- report/src/app/app.component.ts
+++ report/src/app/app.component.ts
@@ -6,12 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  quizScore = '70%';
+  quizTitle = 'My Practice Quiz';
+  quizTopics = 'PHYS, CHEM';
+
   myPracticeQuiz = [
     {
       id: 1,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: Which of the following statements, if true, would most undermine the author’s characterization of Smith?',
+      question: 'Which of the following statements, if true, would most undermine the author’s characterization of Smith?',
       topic: 'PHYS',
       difficulty: 'Easy',
       timeSpent: '20 Sec'
@@ -20,7 +25,7 @@ export class AppComponent {
       id: 2,
       isCorrect: false,
       isBookmarked: false,
-      question: 'Question: According to the author, Smith’s most important contribution to economics was',
+      question: 'According to the author, Smith’s most important contribution to economics was',
       topic: 'CHEM',
       difficulty: 'Easy',
       timeSpent: '90 Sec'
@@ -29,7 +34,7 @@ export class AppComponent {
       id: 3,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: The most widely-used differential stain for bacteria is:',
+      question: 'The most widely-used differential stain for bacteria is:',
       topic: 'PHYS',
       difficulty: 'Easy',
       timeSpent: '20 Sec'
@@ -38,7 +43,7 @@ export class AppComponent {
       id: 4,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: All of the following are true statements about the brain EXCEPT:',
+      question: 'All of the following are true statements about the brain EXCEPT:',
       topic: 'PHYS',
       difficulty: 'Easy',
       timeSpent: '20 Sec'
@@ -47,7 +52,7 @@ export class AppComponent {
       id: 5,
       isCorrect: false,
       isBookmarked: true,
-      question: 'Question: On the subject of the number of hospital beds, the author:',
+      question: 'On the subject of the number of hospital beds, the author:',
       topic: 'PHYS',
       difficulty: 'Hard',
       timeSpent: '60 Sec'
@@ -56,7 +61,7 @@ export class AppComponent {
       id: 6,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: In the Van der Waals equation, the purpose of the n2 is:',
+      question: 'In the Van der Waals equation, the purpose of the n2 is:',
       topic: 'CHEM',
       difficulty: 'Hard',
       timeSpent: '60 Sec'
@@ -65,7 +70,7 @@ export class AppComponent {
       id: 7,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: What is the mechanical advantage of the pulley sytem?',
+      question: 'What is the mechanical advantage of the pulley sytem?',
       topic: 'PHYS',
       difficulty: 'Easy',
       timeSpent: '60 Sec'
@@ -74,7 +79,7 @@ export class AppComponent {
       id: 8,
       isCorrect: false,
       isBookmarked: true,
-      question: 'Question: If the weight moves down 1 cm, how far does the patient’s leg move?',
+      question: 'If the weight moves down 1 cm, how far does the patient’s leg move?',
       topic: 'CHEM',
       difficulty: 'Hard',
       timeSpent: '70 Sec'
@@ -83,7 +88,7 @@ export class AppComponent {
       id: 9,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: Using the VSEPR theory, the shape of PC is:',
+      question: 'Using the VSEPR theory, the shape of PC is:',
       topic: 'CHEM',
       difficulty: 'Easy',
       timeSpent: '60 Sec'
@@ -92,7 +97,7 @@ export class AppComponent {
       id: 10,
       isCorrect: true,
       isBookmarked: false,
-      question: 'Question: The rate law for the reaction is',
+      question: 'The rate law for the reaction is',
       topic: 'PHYS',
       difficulty: 'Easy',
       timeSpent: '10 Sec'
